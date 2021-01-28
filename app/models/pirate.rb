38011e3e29c5e78834f3619c54af30f941bce94f
geraldarzy <geraldarzola@gmail.com>
2021-01-28 18:28:52 -0500

    commit tree a22902073c0f4cc8e8b09a5109f7026237ea4cae

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+    def initialize(hash)
+        @name = hash[:name]
+        @weight = hash[:weight]
+        @height = hash[:height]
+    end
+
+    def self.all
+       @@all 
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.clear
+        self.all.clear
+    end
 end
